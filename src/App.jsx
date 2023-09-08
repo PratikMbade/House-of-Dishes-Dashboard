8dd31199f03944fd4433e3efa9f1c6fd3d161abe
Ne0sky <mailmeakash2@gmail.com>
2023-09-08 20:04:56 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import DashboardPage from './pages/DashboardPage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-       <h1 className="text-3xl font-bold underline font-primary">
-      Hello world!
-    </h1>
-       
-    </>
+    <div>
+    <BrowserRouter>
+      <Navbar/> 
+      <div className='pages'>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/dashboard' element={<DashboardPage/>}/>
+          <Route path='/signup' element={<SignupPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter> 
+    </div>
   )
 }
 
