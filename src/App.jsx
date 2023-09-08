fa2669be6c264d401e7e8c0c80ec57ba23b44587
Ne0sky <mailmeakash2@gmail.com>
2023-09-08 16:16:44 +0530

    add tailwind config for font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
+       <h1 className="text-3xl font-bold underline font-primary">
       Hello world!
     </h1>
        
