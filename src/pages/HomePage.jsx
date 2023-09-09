6b5dc0f4a10cf9c4ffa5160eb9b9f2b5aafe34bd
Ne0sky <mailmeakash2@gmail.com>
2023-09-09 17:30:59 +0530

    fix background blur

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,11 +5,13 @@ import { VscDebugStart } from "react-icons/vsc";
 const HomePage = () => {
   return (
     <div className='bg-black min-h-screen text-white flex flex-col-reverse lg:flex-row md:gap-16 md:items-center justify-center font-primary px-8'>
+      
       <div className='left-section relative w-full md:w-1/2'>
-      <h2 className='font-semibold text-2xl md:text-4xl lg:text-5xl z-10 py-8'><p className='text-amber-500 inline pr-2'>Welcome</p> 
+      <div className='w-[300px] h-[300px] z-1 rounded-full absolute top-[-40px] left-[-40px] blur-3xl z-0 bg-[#1444b339]  '></div>
+      <h2 className='font-semibold relative text-2xl md:text-4xl lg:text-5xl z-10 py-8'><p className='text-amber-500 inline pr-2'>Welcome</p> 
       to House of Dishes <p>Dashboard Chef </p> </h2>
-      <div className='w-[300px] h-[300px]  rounded-full absolute top-[-40px] left-[-40px] blur-3xl  bg-[#b38b1429]  '></div>
-      <p className='description text-lg md:text-xl lg:text-2xl pb-8 '>Discover endless recipes and harness AI guidance for seamless cooking on our website. Elevate your kitchen game today !</p>
+      
+      <p className='description text-lg md:text-xl lg:text-2xl pb-8 relative'>Discover endless recipes and harness AI guidance for seamless cooking on our website. Elevate your kitchen game today !</p>
 
       <Link to='/signup'>
       <button className="get-started group relative px-8 py-3 overflow-hidden font-medium rounded-xl border border-yellow-800  text-xl md:text-2xl shadow-2xl shadow-[#ff910025] mr-8 my-8">
